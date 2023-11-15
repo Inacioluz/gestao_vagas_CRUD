c1368956ad202c1cde147438a034fed70bb44f15
Inacio Silva <joseinaciolds@gmail.com>
2023-11-15 12:05:18 -0300

    formatação de validação

--- src/main/java/br/com/inacio/gestao_vagas/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/inacio/gestao_vagas/candidate/controllers/CandidateController.java
@@ -1,16 +1,18 @@
-package br.com.inacio.gestao_vagas.candidate.controller;
+package br.com.inacio.gestao_vagas.candidate.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
     
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
         System.out.println(candidateEntity.getUsername());
