56a14fcd40dde854fcfa418df38e9f262a7c1080
Inacio Silva <joseinaciolds@gmail.com>
2023-11-15 12:12:12 -0300

    update

--- src/main/java/br/com/inacio/gestao_vagas/candidate/controllers/CandidateEntity.java
+++ src/main/java/br/com/inacio/gestao_vagas/candidate/controllers/CandidateEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -13,11 +14,12 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [email] e o campo [username] não deve conter espaço")
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
     @Email(message = "O campo (email) precisa conter um email válido")
     private String email;
-    @Length(min = 10, max = 20)
+    @Length(min = 10, max = 100, message = "A senha deve ter entre 10 e 10 cracteres")
     private String password;
     private String description;
     private String curriculum;
