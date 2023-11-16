de5099e38c674590a85fba60ebba6a3d31ad6fc4
Inacio Silva <joseinaciolds@gmail.com>
2023-11-16 13:44:14 -0300

    update

--- src/main/java/br/com/inacio/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/inacio/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -15,7 +15,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 @Entity(name = "company")
-static @Data
+@Data
 public class CompanyEntity {
     
 
