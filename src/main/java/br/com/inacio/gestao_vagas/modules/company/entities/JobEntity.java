7abe21ed48f783d7f815ea67946e99608de2fe72
Inacio Silva <joseinaciolds@gmail.com>
2023-12-03 15:47:55 -0300

    refactorando JobController

--- src/main/java/br/com/inacio/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/inacio/gestao_vagas/modules/company/entities/JobEntity.java
@@ -13,29 +13,34 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
 @Entity(name = "job")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-    private String decription;
-    private String benefits;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  private String description;
+  private String benefits;
 
-    @NotBlank(message ="Esse campo é Obrigatório")
-    private String level;
+  @NotBlank(message = "Esse campo é obrigatório")
+  private String level;
 
-    @ManyToOne()
-    @JoinColumn(name = "company_id", insertable = false, updatable = false)
-    private CompanyEntity companyEntity;
+  @ManyToOne()
+  @JoinColumn(name = "company_id", insertable = false, updatable = false)
+  private CompanyEntity companyEntity;
 
-    @Column(name = "company_id", nullable = false)
-    private UUID companyId;
+  @Column(name = "company_id", nullable = false)
+  private UUID companyId;
 
-
-    @CreationTimestamp
-    private LocalDateTime createdAt;
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
